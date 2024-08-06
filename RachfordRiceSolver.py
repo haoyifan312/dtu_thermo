978ebc5fefee9a2e85ba60e1c9b0a4573e376eb5
yifan.hao <haoyifanzz312@gmail.com>
2024-08-06 16:35:23 -0400

    Rachford-Rice solver with sloppy option

---
+++ RachfordRiceSolver.py
@@ -0,0 +1,179 @@
+import dataclasses
+from enum import IntEnum
+from typing import List
+import numpy as np
+
+
+class RachfordRiceException(Exception):
+    pass
+
+
+@dataclasses.dataclass
+class RachfordRiceResult:
+    beta: float = -1.0
+    xs: np.array = None
+    ys: np.array = None
+    betas: List = dataclasses.field(default_factory=list)
+
+    def clear(self):
+        if self.xs is None:
+            raise RachfordRiceException('Cannot clear Rachford-Rice result that is not initialized')
+
+        self.beta = -1.0
+        self.xs[:] = 0.0
+        self.ys[:] = 0.0
+        self.betas.clear()
+
+    def set_as_liquid(self, zs, beta=0.0, ys=None):
+        self.beta = beta
+        self.xs = zs.copy()
+        if ys:
+            self.ys = ys
+        else:
+            self.ys[:] = 0.0
+
+    def set_as_vapor(self, zs: np.array, beta=1.0, xs=None):
+        self.beta = beta
+        if xs:
+            self.xs = xs
+        else:
+            self.xs[:] = 0.0
+        self.ys = zs.copy()
+
+
+def raise_max_iter_exception(max_iter: int):
+    raise RachfordRiceException(f'Rachford-Rice solver did not converge in {max_iter} iterations')
+
+
+def max_iter_dummy(max_iter):
+    pass
+
+class RachfordRiceGSolver:
+    def __init__(self, max_iter=100, max_iter_fun=raise_max_iter_exception):
+        self._ks = None
+        self._zs = None
+        self._max_iter = max_iter
+        self._max_iter_fun = max_iter_fun
+
+    def set_input(self, ks, zs):
+        self._ks = ks
+        self._zs = zs
+
+    def solve(self, result: RachfordRiceResult):
+        beta_min = 0.0
+        beta_max = 1.0
+
+        beta_new = 0.5
+        for i in range(self._max_iter):
+            g = self.fun(beta_new)
+            result.betas.append(beta_new)
+            if g < 0.0:
+                beta_max = beta_new
+            else:
+                beta_min = beta_new
+
+            g_prime = self.der(beta_new)
+            if abs(g_prime) < 1e-20:
+                break
+            newton_step = - g / g_prime
+            if self._solver_reached_solution(g, newton_step):
+                break
+
+            beta_new += newton_step
+            if beta_new > beta_max or beta_new < beta_min:
+                beta_new = (beta_min + beta_max) / 2.0
+        else:
+            self._max_iter_fun(self._max_iter)
+
+        self._fill_result(beta_new, result)
+
+    def fun(self, beta):
+        ret = 0.0
+        for zi, ki in zip(self._zs, self._ks):
+            ret += zi * (ki - 1.0) / (1.0 - beta + beta * ki)
+        return ret
+
+    def der(self, beta):
+        ret = 0.0
+        for zi, ki in zip(self._zs, self._ks):
+            ret -= zi * (ki - 1.0) ** 2 / (1.0 - beta + beta * ki) ** 2
+        return ret
+
+    @staticmethod
+    def _solver_reached_solution(g, newton_step):
+        return abs(g) < 1e-6 and abs(newton_step) < 1e-6
+
+    def _fill_result(self, beta, result):
+        result.beta = beta
+        for i, (ki, zi) in enumerate(zip(self._ks, self._zs)):
+            denom = 1.0 - beta + beta * ki
+            result.xs[i] = zi / denom
+            result.ys[i] = ki * zi / denom
+
+
+class RachfordRiceGSolverSloppy(RachfordRiceGSolver):
+    def _fill_result(self, beta, result):
+        super()._fill_result(beta, result)  # x, y are li, vi
+        result.xs = result.xs*(1 - beta)
+        result.ys = result.ys*beta
+
+        sumv = sum(result.ys)
+        result.xs = result.xs/(1.0-sumv)
+        result.ys = result.ys/sumv
+
+
+
+class RachfordRiceSolverOption(IntEnum):
+    BASE = 0
+    SLOPPY = 1
+    NEGATIVE_FLASH = 2
+
+
+class RachfordRiceBase:
+    def __init__(self, size, g_solver: RachfordRiceGSolver):
+        self._size = size
+        self._result = RachfordRiceResult()
+        self._initialize_result()
+        self._g_solver = g_solver
+
+    @property
+    def size(self):
+        return self._size
+
+    def _initialize_result(self):
+        self._result.xs = np.zeros(self.size)
+        self._result.ys = np.zeros(self.size)
+
+    def compute(self, ks, zs):
+        self._result.clear()
+        self._check_input_sizes(ks, zs)
+        self._g_solver.set_input(ks, zs)
+        if self._require_solving_beta(zs):
+            self._g_solver.solve(self._result)
+        return self._result
+
+    def _require_solving_beta(self, zs):
+        g0 = self._g_solver.fun(0.0)
+        g1 = self._g_solver.fun(1.0)
+        if g0 > 0.0 and g1 > 0.0:
+            self._result.set_as_vapor(zs)
+            return False
+        elif g0 < 0.0 and g1 < 0.0:
+            self._result.set_as_liquid(zs)
+            return False
+        else:
+            return True
+
+    def _check_input_sizes(self, ks, zs):
+        if len(ks) != len(zs) or len(ks) != self.size:
+            raise RachfordRiceException('Rachford-Rice input size incorrect')
+
+    @staticmethod
+    def create_solver(size: int, option: RachfordRiceSolverOption):
+        if option == RachfordRiceSolverOption.BASE:
+            return RachfordRiceBase(size, RachfordRiceGSolver())
+        elif option == RachfordRiceSolverOption.SLOPPY:
+            return RachfordRiceBase(size, RachfordRiceGSolverSloppy(max_iter=1, max_iter_fun=max_iter_dummy))
+
+
+
--- test/test_day2.py
+++ test/test_day2.py
@@ -1,4 +1,9 @@
+import math
 import unittest
+
+import numpy as np
+
+from RachfordRiceSolver import RachfordRiceBase, RachfordRiceSolverOption
 from thermclc_interface import *
 
 
@@ -14,7 +19,7 @@ class TestWilsonK(unittest.TestCase):
                 tc, pc, omega = stream.get_critical_properties(i)
                 # print(f'{comp}: TC={tc}\tPC={pc}\t\tOmega={omega}')
                 self.assertEqual(tc, self.tc_gold[i])
-                self.assertEqual(pc, self.pc_gold[i]*0.1013)
+                self.assertEqual(pc, self.pc_gold[i] * 0.1013)
                 self.assertEqual(omega, self.omega_gold[i])
 
     def test_wilson_k(self):
@@ -24,21 +29,127 @@ class TestWilsonK(unittest.TestCase):
             for i, comp in enumerate(self.components):
                 k = stream.compute_wilson_k(t, p, i)
                 print(f'Wilson K for {comp} at {t}K and {p}MPa = {k}')
-                k_compare = compute_wilson_k(t, self.tc_gold[i], p, self.pc_gold[i]*0.1013, self.omega_gold[i])
+                k_compare = compute_wilson_k(t, self.tc_gold[i], p, self.pc_gold[i] * 0.1013, self.omega_gold[i])
                 self.assertAlmostEqual(k, k_compare)
 
 
 class TestRachfordRiceSolver(unittest.TestCase):
     components = list(example_7_component.keys())
-    z = np.array(list(example_7_component.values()))
+    zs = np.array(list(example_7_component.values()))
     p_mpa = 5.0
+    ts = [190.0, 195, 200.0, 220.0, 280.0, 300.0, 350.0]
+    beta_gold = [-0.1792, 0.3176, 0.7712, 0.9521, 0.9980, 1.0027, 1.0253]
 
     @property
     def size(self):
         return len(self.components)
 
+    def create_solver(self, option=RachfordRiceSolverOption.BASE):
+        return RachfordRiceBase.create_solver(self.size, option)
+
     def test_create_solver(self):
-        solver = RachfordRiceBase(self.size)
+        solver = self.create_solver()
+        self.assertEqual(solver.size, self.size)
+        self.assertEqual(solver._result.beta, -1.0)
+        self.assertTrue(np.allclose(solver._result.xs, np.zeros(self.size)))
+        self.assertTrue(np.allclose(solver._result.ys, np.zeros(self.size)))
 
+    def test_solve_liquid(self):
+        t = self.ts[0]
+        solver = self.create_solver()
+        with init_system(self.components, 'SRK') as stream:
+            ks = [stream.compute_wilson_k(t, self.p_mpa, i) for i in range(self.size)]
+            result = solver.compute(ks, self.zs)
+            print(result)
+            self.assertEqual(result.beta, 0.0)
+            self.assertTrue(np.allclose(result.xs, self.zs))
+            self.assertTrue(np.allclose(result.ys, np.zeros(self.size)))
 
+    def test_solve_vapor(self):
+        t = self.ts[-1]
+        solver = self.create_solver()
+        with init_system(self.components, 'SRK') as stream:
+            ks = [stream.compute_wilson_k(t, self.p_mpa, i) for i in range(self.size)]
+            result = solver.compute(ks, self.zs)
+            print(result)
+            self.assertEqual(result.beta, 1.0)
+            self.assertTrue(np.allclose(result.ys, self.zs))
+            self.assertTrue(np.allclose(result.xs, np.zeros(self.size)))
 
+    def test_solve_vle(self):
+        i = 1
+        t = self.ts[i]
+        solver = self.create_solver()
+        with init_system(self.components, 'SRK') as stream:
+            ks = [stream.compute_wilson_k(t, self.p_mpa, i) for i in range(self.size)]
+            result = solver.compute(ks, self.zs)
+            print(result)
+            self.assertAlmostEqual(result.beta, self.beta_gold[i], places=4)
+            self.assertTrue(np.isclose(np.sum(result.xs), 1.0))
+            self.assertTrue(np.isclose(np.sum(result.ys), 1.0))
+
+    def test_solve_all(self):
+        solver = self.create_solver()
+        with init_system(self.components, 'SRK') as stream:
+            for i, t in enumerate(self.ts):
+                ks = [stream.compute_wilson_k(t, self.p_mpa, i) for i in range(self.size)]
+                result = solver.compute(ks, self.zs)
+                print(f'\nT={t}:')
+                print(result)
+                beta_gold = self.beta_gold[i]
+                total_x = 1.0
+                total_y = 1.0
+                if beta_gold > 1.0:
+                    beta_gold = 1.0
+                    total_x = 0.0
+                elif beta_gold < 0.0:
+                    beta_gold = 0.0
+                    total_y = 0.0
+                self.assertAlmostEqual(result.beta, beta_gold, places=4)
+                self.assertTrue(np.isclose(np.sum(result.xs), total_x))
+                self.assertTrue(np.isclose(np.sum(result.ys), total_y))
+
+    def test_convergence_pattern(self):
+        """
+        last few steps are quadratically converging
+        """
+        solver = self.create_solver()
+        with init_system(self.components, 'SRK') as stream:
+            for i in (1, 2, 3, 4):
+                t = self.ts[i]
+                ks = [stream.compute_wilson_k(t, self.p_mpa, i) for i in range(self.size)]
+                result = solver.compute(ks, self.zs)
+                betas = result.betas.copy()
+                log_steps = []
+                log_step_diffs = []
+                for i in range(1, len(betas)):
+                    this_beta = betas[i]
+                    last_beta = betas[i - 1]
+                    log_steps.append(math.log(abs(this_beta - last_beta)))
+                    if len(log_steps) > 1:
+                        log_step_diffs.append(log_steps[i - 1] - 2 * log_steps[i - 2])
+
+                print(log_steps)
+                print(log_step_diffs)
+
+    def test_solve_all_sloppy(self):
+        solver = self.create_solver(RachfordRiceSolverOption.SLOPPY)
+        beta_golds = np.array([0.0, 0.3505, 0.9749, 0.75, 0.75, 1.0, 1.0])
+        with init_system(self.components, 'SRK') as stream:
+            for i, t in enumerate(self.ts):
+                ks = [stream.compute_wilson_k(t, self.p_mpa, i) for i in range(self.size)]
+                result = solver.compute(ks, self.zs)
+                print(f'\nT={t}:')
+                print(result)
+                total_x = 1.0
+                total_y = 1.0
+                beta_gold = beta_golds[i]
+                if beta_gold == 1.0:
+                    beta_gold = 1.0
+                    total_x = 0.0
+                elif beta_gold == 0.0:
+                    beta_gold = 0.0
+                    total_y = 0.0
+                self.assertAlmostEqual(result.beta, beta_gold, places=3)
+                self.assertTrue(np.isclose(np.sum(result.xs), total_x))
+                self.assertTrue(np.isclose(np.sum(result.ys), total_y))
