b2f8c56fcef023ec6bd306cfae83eaeb7ebba22c
yifan.hao <haoyifanzz312@gmail.com>
2024-08-16 23:47:17 -0400

    chemical equil: added functionalities to compute K, mu, x

--- ReactionBuilder.py
+++ ReactionBuilder.py
@@ -10,11 +10,13 @@ class ComponentType(IntEnum):
     DIMER = 1
     INERT = 2
 
+
 @dataclasses.dataclass
 class Component:
     name: str
     type: ComponentType
     material_balance_group_and_stoi: List
+
 
 class ReactionBuilder:
     def __init__(self, app_components: list):
@@ -22,6 +24,54 @@ class ReactionBuilder:
         self._mbg = self._extract_material_group()
         self._true_components = self._build_true_components()
         self._mbg_by_component_matrix = self._build_material_group_by_true_components_matrix()
+        self.keq_data = None
+        self.mu_by_rt = np.zeros(len(self._true_components))
+        self._keq_index = self._create_keq_index()
+        self._keq_per_mmhg_values = np.zeros(len(self.dimers))
+        self._lambdas = np.zeros(len(app_components))
+        self._bi = np.zeros(len(app_components))
+        self._xi = np.zeros(len(self._true_components))
+
+    def set_bi_from_zi(self, zi):
+        self._bi = zi
+
+    def _update_xi(self):
+        first_term = np.matmul(np.transpose(self._mbg_by_component_matrix), np.transpose(self._lambdas))
+        ln_xi = np.transpose(first_term) - self.mu_by_rt
+        self._xi = np.exp(ln_xi)
+
+    def set_keqs(self, keqs: dict):
+        self.keq_data = keqs
+
+    def update_keqs(self, t):
+        dimer_keqs = {name: self._compute_keq(data, t)
+                                for name, data in self.keq_data.items()}
+        self_dimers = list(dimer_keqs.keys())
+        self_dimer_size = len(self_dimers)
+        for i in range(self_dimer_size):
+            for j in range(i+1, self_dimer_size):
+                self_dimer_i = self_dimers[i]
+                self_dimer_j = self_dimers[j]
+                cross_dimer_name = self_dimer_i[0] + self_dimer_j[0]
+                dimer_keqs[cross_dimer_name] = 2.0*np.sqrt(dimer_keqs[self_dimer_i]*dimer_keqs[self_dimer_j])
+
+        for dimer, keq in dimer_keqs.items():
+            self._keq_per_mmhg_values[self._keq_index[dimer]] = keq
+
+    def _compute_keq(self, data, t):
+        k1, k2 = data
+        return np.pow(10.0, k1 + k2/t)
+
+    def update_mu_by_rt(self, t, p_atm):
+        self.update_keqs(t)
+        p_mmhg = p_atm * 760
+        self.mu_by_rt = np.zeros(len(self._true_components))
+        for i, component in enumerate(self._true_components):
+            if component.type != ComponentType.DIMER:
+                continue
+            keq_index = self._keq_index[component.name]
+            keq = self._keq_per_mmhg_values[keq_index]
+            self.mu_by_rt[i] = -np.log(keq*p_mmhg)
 
     @property
     def app_component_names(self):
@@ -30,6 +80,14 @@ class ReactionBuilder:
     @property
     def monomers(self):
         return [c for c in self.app_components if c.type == ComponentType.MONOMER]
+
+    @property
+    def dimers(self):
+        return [c for c in self._true_components if c.type == ComponentType.DIMER]
+
+    @property
+    def dimer_names(self):
+        return [c.name for c in self._true_components if c.type == ComponentType.DIMER]
 
     def _build_true_components(self):
         monomers = self.monomers
@@ -72,4 +130,6 @@ class ReactionBuilder:
                 ret.add(mbg_name)
         return list(ret)
 
+    def _create_keq_index(self):
+        return {name: i for i, name in enumerate(self.dimer_names)}
 
--- test/test_day9.py
+++ test/test_day9.py
@@ -1,9 +1,18 @@
 import unittest
 
+import numpy as np
+
 from ReactionBuilder import ComponentType, Component, ReactionBuilder
 
 
 class TestReactionBuilder(unittest.TestCase):
+    keq_data = {
+        'AA': (-10.743, 3083),
+        'BB': (-10.421, 3166),
+        'CC': (-10.843, 3316),
+        'DD': (-10.136, 3079)
+    }
+
     def test_components(self):
         A = Component('A', ComponentType.MONOMER, [('A', 1)])
         AB = Component('AB', ComponentType.DIMER, [('A', 1), ('B', 1)])
@@ -24,6 +33,28 @@ class TestReactionBuilder(unittest.TestCase):
         self.assertTrue(sorted(true_components), sorted(builder.true_component_names))
         self.assertEqual(len(builder.true_component_names), 15)
         print(builder._mbg_by_component_matrix)
+
+    def test_keq_and_mu(self):
+        t = 350
+        p = 1
+        builder = self._build_example_reaction_system()
+        builder.set_keqs(self.keq_data)
+        builder.update_keqs(t)
+        print(builder._keq_per_mmhg_values)
+        builder.update_mu_by_rt(t, p)
+        print(builder.mu_by_rt)
+
+    def test_ln_xi(self):
+        t = 350
+        p = 1
+        builder = self._build_example_reaction_system()
+        builder.set_keqs(self.keq_data)
+        builder.update_mu_by_rt(t, p)
+
+        builder.set_bi_from_zi(np.array([0.2]*5))
+        builder._lambdas = np.array([-1]*5)
+        builder._update_xi()
+        print(builder._xi)
 
     def _build_example_reaction_system(self):
         monomers = [Component(name, ComponentType.MONOMER, [(name, 1)])
